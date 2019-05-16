f26d5a67f2720374ffb02ba5cef6cadcb814a79e
Hugh <hghklly@gmail.com>
2019-05-16 16:18:08 +0000

    forgot

--- Server/DataServer.js
+++ Server/DataServer.js
@@ -454,7 +454,7 @@ app.get('/timeline/group', function (req, res) {
       res.header("Access-Control-Allow-Headers", "X-Requested-WithD");
       // If all the variables are provided connect to the database
 
- var sql = "SELECT country, count FROM donation_data WHERE country = 'United States' OR country = 'United Kingdom' OR country = 'Japan' OR country = 'Egypt' OR country = 'Zaire' OR country = 'Australia';
+ var sql = "SELECT country, count FROM donation_data WHERE country = 'United States' OR country = 'United Kingdom' OR country = 'Japan' OR country = 'Egypt' OR country = 'Zaire' OR country = 'Australia'";
           
 
 /////////////////////////////////////////////////////////////////
